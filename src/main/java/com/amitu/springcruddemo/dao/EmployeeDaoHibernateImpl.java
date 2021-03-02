ab6532dd53f9abba3a62471d162c8ff682addd94
amitu <amitu@cmj-madhuri.cdacmumbai.in>
2021-03-02 19:43:04 +0530

    Commit from eclipse

--- src/main/java/com/amitu/springcruddemo/dao/EmployeeDaoHibernateImpl.java
+++ src/main/java/com/amitu/springcruddemo/dao/EmployeeDaoHibernateImpl.java
@@ -2,9 +2,22 @@ package com.amitu.springcruddemo.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.amitu.springcruddemo.entity.Employee;
 
+@Repository
 public class EmployeeDaoHibernateImpl implements EmployeeDao {
+	
+	private EntityManager entityManager;
+	
+	@Autowired
+	public EmployeeDaoHibernateImpl(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
 
 	@Override
 	public List<Employee> findAll() {
